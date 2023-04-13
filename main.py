4ebce65dafc6aa4c6df6c9e82a45df71acf04ea0
Fr0stFree <me@frostfree.ru>
2023-04-13 11:01:20 +0300

    add comments, fix typos

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import datetime as dt
 import io
 from pathlib import Path
-from typing import Iterator, AsyncGenerator
+from typing import Iterator, AsyncGenerator, Generator
 
 import aiofiles
 import aiohttp
@@ -12,25 +12,24 @@ from bs4 import BeautifulSoup
 
 
 BASE_DIR: Path = Path(__file__).parent
-STORAGE_DIR: Path = BASE_DIR / 'storage'
+BASE_URL: str = 'https://www.ncei.noaa.gov/data/global-summary-of-the-day/access/'
 SEMAPHORE_LIMIT: int = 5
-
-
 with open(BASE_DIR / 'stations.txt') as f:
     STATIONS_IDS = [station_id for station_id in f.read().split()]
 
 
-def parse_page_to_links(html: str) -> Iterator[str]:
+def parse_page_to_links(html: str) -> Generator[str]:
     soup = BeautifulSoup(html, "html.parser")
     table = soup.find("table")
     return (link["href"] for link in table.find_all("a"))
 
 
-def filter_links(links: Iterator[str]) -> Iterator[str]:
+def filter_links(links: Generator[str]) -> Iterator[str]:
     return filter(lambda link: any([link.startswith(station_id) for station_id in STATIONS_IDS]), links)
 
 
-async def download_csv(link: str, session: aiohttp.ClientSession, semaphore: asyncio.Semaphore):
+async def download_csv(link: str, session: aiohttp.ClientSession,
+                       semaphore: asyncio.Semaphore) -> str:
     async with semaphore:
         async with session.get(link) as response:
             response.raise_for_status()
@@ -65,22 +64,25 @@ def df_to_feature(data: pd.DataFrame) -> geojson.Feature:
     
 
 async def main(looking_date: dt.datetime):
-    page_url: str = f'https://www.ncei.noaa.gov/data/global-summary-of-the-day/access/{looking_date.year}/'
+    page_url = f'{BASE_URL}{looking_date.year}/'
     feature_collection = geojson.FeatureCollection([])
 
+    # Download base page
     async with aiohttp.ClientSession() as session:
         async with session.get(page_url) as response:
-            html = await response.text()
+            html = await response.text(encoding='utf-8')
 
-        links = parse_page_to_links(html)
-        looking_links = filter_links(links)
+        # Parsing page into filtered links
+        links: Iterator[str] = filter_links(parse_page_to_links(html))
 
-        async for csv_file in bulk_download_csv(looking_links, page_url, session):
+        # Download datasets from every link and add it to feature collection
+        async for csv_file in bulk_download_csv(links, page_url, session):
             row: pd.DataFrame = find_row_by_date(csv_file, looking_date)
             if not row.empty:
                 feature_collection.features.append(df_to_feature(row))
 
-    async with aiofiles.open(STORAGE_DIR / 'result.geojson', "w", encoding="utf-8") as f:
+    # Save result
+    async with aiofiles.open(BASE_DIR / 'result.geojson', "w", encoding="utf-8") as f:
         await f.write(geojson.dumps(feature_collection, indent=4))
     
         
